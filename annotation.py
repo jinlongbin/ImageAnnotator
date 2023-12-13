cfa6b6a201c5c1118820c8a5e2c091af528efad2
Jin Longbin <51145547+jinlongbin@users.noreply.github.com>
2023-12-13 16:17:51 +0900

    Update annotation.py

--- annotation.py
+++ annotation.py
@@ -44,6 +44,7 @@ class ImageAnnotator:
             self.children = self.load_children(self.single_folder_path)
             self.image_files = os.listdir(self.group_folder_path)
             self.image_files = [file for file in self.image_files if file.endswith(('.jpeg', '.jpg', '.png'))]
+            self.image_files = sorted(self.image_files, key=lambda x: x.split('.')[0])
             self.setup_annotation_controls()
             self.load_image()
             self.select_folder_button.destroy()  # Remove the select folder button
